3cfe113060af817023ad234b53bb325d439f4b24
kevsaba <46569109+kevsaba@users.noreply.github.com>
2021-11-12 17:55:59 +0100

    pet clinic controller and index. Closes #7

---
+++ pet-clinic-web/src/main/java/kev/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package kev.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String listOwners(Model model) {
+        return "owner/index";
+    }
+}
