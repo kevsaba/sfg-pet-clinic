9f4d0eb7f7c13c63a3c3232db5d9b3c04a9268dd
kevsaba <46569109+kevsaba@users.noreply.github.com>
2021-11-29 19:15:43 +0100

    add spring data jpa services to handle crud repository. Closes #28

---
+++ pet-clinic-data/src/main/java/kev/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package kev.springframework.sfgpetclinic.services.springdatajpa;
+
+import kev.springframework.sfgpetclinic.model.Vet;
+import kev.springframework.sfgpetclinic.repositories.VetRepository;
+import kev.springframework.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        final var vets = new HashSet<Vet>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
