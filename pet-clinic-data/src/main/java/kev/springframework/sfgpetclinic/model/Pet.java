8df52717c2802a088bc538bef1d3700c0133377b
kevsaba <46569109+kevsaba@users.noreply.github.com>
2021-11-29 18:45:01 +0100

    JPA Entities annotations setup.

--- pet-clinic-data/src/main/java/kev/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/kev/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package kev.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -18,6 +20,9 @@ public class Pet extends BaseEntity{
     private PetType petType;
     @Column(name = "birdate")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public PetType getPetType() {
         return petType;
