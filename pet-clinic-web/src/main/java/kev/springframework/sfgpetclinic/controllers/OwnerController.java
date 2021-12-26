88deb7be7ded4b873c7de4872c6b153bd633827e
kevsaba <46569109+kevsaba@users.noreply.github.com>
2021-12-26 18:11:05 +0100

    to control the data binding or deserialization of the json objects/forms into java objects. Closes #48

--- pet-clinic-web/src/main/java/kev/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kev/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package kev.springframework.sfgpetclinic.controllers;
 import kev.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +20,15 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/index", "/index.html"})
+    //its to have a control over how the "magic" its done about the web data bind into java objects (deserialization)
+    //here we are disallowing id because we are doing defensing coding, so the web forms for example can not address or manipulate
+    //the id property of objects stored in the database, to avoid people to hack the db
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @GetMapping({"", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
