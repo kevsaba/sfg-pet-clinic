4b064451eaf8ad5ecdf5d07fa02a6e615a950ef3
kevsaba <46569109+kevsaba@users.noreply.github.com>
2021-12-29 16:04:45 +0100

    view vet list, this also broke the ui some how so i might double check later

--- pet-clinic-web/src/main/java/kev/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/kev/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package kev.springframework.sfgpetclinic.controllers;
 
+import kev.springframework.sfgpetclinic.model.Vet;
 import kev.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -13,9 +17,16 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets/index","/vets.html"})
+    @GetMapping({"/vets", "/vets/index", "/vets.html"})
     public String listVets(Model model) {
-        model.addAttribute("vets",vetService.findAll());
-        return "vets/index";
+        model.addAttribute("listVets", vetService.findAll());
+        return "vets/vetList";
     }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
+
 }
